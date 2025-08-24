0d5b80c3771b8a2bf45d699d53681dca7cdc986e
Sara <sarabeytuie@gmail.com>
2025-08-24 20:34:09 +0330

    feat(jbook): transpile code

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,33 @@
-import { useState } from "react";
+import * as esbuild from 'esbuild-wasm';
+import { useEffect, useRef, useState } from "react";
 
 function App() {
-  const [input, setInput] = useState("");
-  const [code, setCode] = useState("");
+  const ref = useRef<any>(null);
+  const [input, setInput] = useState('');
+  const [code, setCode] = useState('');
+
+  const startService = async () => {
+    ref.current = await esbuild.startService({
+      worker: true,
+      wasmURL: '../public/esbuild.wasm',
+    });
+  };
+
+  useEffect(() => {
+    startService();
+  }, []);
 
   const onClick = async () => {
-    console.log("on click!");
+    if (!ref.current) {
+      return;
+    }
+
+    const result = await ref.current.transform(input, {
+      loader: 'jsx',
+      target: 'es2015'
+    });
+
+    setCode(result.code);
   };
   
   return (
