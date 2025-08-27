d7a7334cfe8e5dea97176b797babbf95c7228519
Sara <sarabeytuie@gmail.com>
2025-08-27 15:49:31 +0330

    feat(jbook): refactore and use seperate plugins

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import * as esbuild from 'esbuild-wasm';
 import { useEffect, useRef, useState } from "react";
 import { unpkgPathPlugin } from './plugins/unpkg-path-plugin';
+import { fetchPlugin } from './plugins/fetch-plugin';
 
 function App() {
   const ref = useRef<any>(null);
@@ -10,7 +11,7 @@ function App() {
   const startService = async () => {
     ref.current = await esbuild.startService({
       worker: true,
-      wasmURL: '../public/esbuild.wasm',
+      wasmURL: 'https://unpkg.com/esbuild-wasm@0.8.27/esbuild.wasm',
     });
   };
 
@@ -27,7 +28,11 @@ function App() {
       entryPoints: ['index.js'],
       bundle: true,
       write: false,
-      plugins: [unpkgPathPlugin()],
+      plugins: [unpkgPathPlugin(), fetchPlugin(input)],
+      define: {
+        'process.env.NODE_ENV': '"production"',
+        global: 'window',
+      },
     });
 
     setCode(result.outputFiles[0].text);
