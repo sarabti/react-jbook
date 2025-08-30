f950ddfc185f5a5537ac8cf80735edd0d3b692b9
Sara <sarabeytuie@gmail.com>
2025-08-31 00:20:38 +0330

    feat(jbook): add format button and some styles

--- src/components/code-editor.tsx
+++ src/components/code-editor.tsx
@@ -1,32 +1,66 @@
-import MonacoEditor from "@monaco-editor/react";
+import "./code-editor.css";
+import MonacoEditor, { type OnMount } from "@monaco-editor/react";
+import prettier from 'prettier';
+import parserBabel from "prettier/plugins/babel";
+import parserEstree from "prettier/plugins/estree";
+import { useRef } from "react";
 
 interface ICodeEditorProps {
   initialValue: string;
   onChange(value: string): void;
 }
 const CodeEditor: React.FC<ICodeEditorProps> = ({ initialValue, onChange }) => {
+  const editorRef = useRef<any>(null);
+
+  const setEditorRef: OnMount = (editor) => editorRef.current = editor
+
   const handleEditorChange = (value: string | undefined) =>
     onChange(value ?? "");
 
+  const onFormatClick = async () => {
+    const unformatted = editorRef.current.getModel().getValue();
+
+    const formatted = (await prettier
+      .format(unformatted, {
+        parser: 'babel',
+        plugins: [parserBabel, parserEstree],
+        useTabs: false,
+        semi: true,
+        singleQuote: true,
+      }))
+      .replace(/\n$/, '');
+
+    editorRef.current.setValue(formatted);
+  };
+
   return (
-    <MonacoEditor
-      value={initialValue}
-      onChange={handleEditorChange}
-      theme="vs-dark"
-      language="javascript"
-      height="500px"
-      options={{
-        wordWrap: "on",
-        minimap: { enabled: false },
-        showUnused: false,
-        folding: false,
-        lineNumbersMinChars: 3,
-        fontSize: 16,
-        scrollBeyondLastLine: false,
-        automaticLayout: true,
-        tabSize: 2,
-      }}
-    />
+    <div className="editor-wrapper">
+      <button
+        className="button button-format is-primary is-small"
+        onClick={onFormatClick}
+      >
+        Format
+      </button>
+      <MonacoEditor
+        value={initialValue}
+        onChange={handleEditorChange}
+        onMount={setEditorRef}
+        theme="vs-dark"
+        language="javascript"
+        height="500px"
+        options={{
+          wordWrap: "on",
+          minimap: { enabled: false },
+          showUnused: false,
+          folding: false,
+          lineNumbersMinChars: 3,
+          fontSize: 16,
+          scrollBeyondLastLine: false,
+          automaticLayout: true,
+          tabSize: 2,
+        }}
+      />
+    </div>
   );
 };
 
