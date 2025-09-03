0e40e137af500708ffa87a04f75ae3bce7e57c5b
Sara <sarabeytuie@gmail.com>
2025-09-03 11:43:16 +0330

    feat(jbook): extract and refactor

---
+++ src/components/preview.tsx
@@ -0,0 +1,62 @@
+import { useRef, useEffect } from "react";
+
+interface PreviewProps {
+  code: string;
+}
+
+const html = `
+  <html>
+    <head></head>
+    <body>
+      <div id="root"></div>
+      <script>
+        window.addEventListener('message', (event) => {
+          try {
+            eval(event.data);
+          } catch (err) {
+            const root = document.querySelector('#root');
+            root.innerHTML = '<div style="color: red;"><h4>Runtime Error</h4>' + err + '</div>';
+            console.error(err);
+          }
+        }, false);
+      </script>
+    </body>
+  </html>
+`;
+
+const Preview: React.FC<PreviewProps> = ({ code }) => {
+  const iframe = useRef<any>(null);
+
+  useEffect(() => {
+    if (!iframe.current) {
+      return;
+    }
+    iframe.current.srcdoc = html;
+
+    const onIframeLoad = () => {
+      if (iframe.current) {
+        iframe.current.contentWindow?.postMessage(code, "*");
+      }
+    };
+
+    iframe.current.addEventListener("load", onIframeLoad);
+
+    return () => {
+      if (iframe.current) {
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+        iframe.current.removeEventListener("load", onIframeLoad);
+      }
+    };
+  }, [code]);
+
+  return (
+    <iframe
+      title="preview"
+      ref={iframe}
+      sandbox="allow-scripts"
+      srcDoc={html}
+    />
+  );
+};
+
+export default Preview;
