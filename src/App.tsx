d6faa52b67f514aab2f4737d22eeee5e34cc316c
Sara <sarabeytuie@gmail.com>
2025-09-06 23:24:03 +0330

    feat(jbook): redux scaffolding

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import TextEditor from "./components/text-editor";
+import { Provider } from "react-redux";
+import { store } from "./state";
 
 function App() {
   return (
-    <>
+    <Provider store={store}>
       {/* <CodeCell /> */}
       <TextEditor />
-    </>
+    </Provider>
   );
 }
 
