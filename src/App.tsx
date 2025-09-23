724ba4a9cad4a8533bd8fb0645c4743ac55494cf
Sara <sarabeytuie@gmail.com>
2025-09-23 23:00:00 +0330

    feat(jbook): add hooks for actions

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import TextEditor from "./components/text-editor";
 import { Provider } from "react-redux";
 import { store } from "./state";
+import CellList from "./components/cell-list";
 
 function App() {
   return (
--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import 'bulmaswatch/superhero/bulmaswatch.min.css';
+import '@fortawesome/fontawesome-free/css/all.min.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
